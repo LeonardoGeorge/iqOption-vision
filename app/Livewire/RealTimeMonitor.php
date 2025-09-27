8235e5ec8013c436825200bed6c2427ede6e7578
Leonardo George <leonardogeorge30@gmail.com>
2025-09-27 09:20:05 -0300

    first commit

---
+++ app/Livewire/RealTimeMonitor.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class RealTimeMonitor extends Component
+{
+    public function render()
+    {
+        return view('livewire.real-time-monitor');
+    }
+}
---
+++ app/Livewire/TradingPanel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class TradingPanel extends Component
+{
+    public function render()
+    {
+        return view('livewire.trading-panel');
+    }
+}
